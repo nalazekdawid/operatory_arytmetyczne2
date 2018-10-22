5f9f91806cd36410ab37e53c91747c483175e209
azbest <azbes@gmail.com>
2018-10-22 21:31:35 +0200

    podstawowy najprostrzy kalkulator

--- operatory_arytmetyczne/MainWindow.xaml.cs
+++ operatory_arytmetyczne/MainWindow.xaml.cs
@@ -24,5 +24,70 @@ namespace operatory_arytmetyczne
         {
             InitializeComponent();
         }
+        
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_Initialized(object sender, EventArgs e)
+        {
+            textBlock4.Text = textBlock5.Text = "";
+            
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            dzialanie(Convert.ToDouble(textBox.Text), Convert.ToDouble(textBox1.Text), 0);
+        }
+
+        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            dzialanie(Convert.ToDouble(textBox.Text), Convert.ToDouble(textBox1.Text), 1);
+        }
+
+        private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+            dzialanie(Convert.ToDouble(textBox.Text), Convert.ToDouble(textBox1.Text), 2);
+        }
+
+        private void radioButton3_Checked(object sender, RoutedEventArgs e)
+        {
+            dzialanie(Convert.ToDouble(textBox.Text), Convert.ToDouble(textBox1.Text), 3);
+        }
+
+        private void radioButton4_Checked(object sender, RoutedEventArgs e)
+        {
+            dzialanie(Convert.ToDouble(textBox.Text), Convert.ToDouble(textBox1.Text), 4);
+        }
+        private void dzialanie (double arg1, double arg2, int zadanie)
+        {
+            if (zadanie == 0)
+            {
+                textBlock4.Text = textBox.Text + " + " + textBox1.Text;
+                textBlock5.Text = Convert.ToString(arg1 + arg2);
+            }
+            else if (zadanie == 1)
+            {
+                textBlock4.Text = textBox.Text + " - " + textBox1.Text;
+                textBlock5.Text = Convert.ToString(arg1 - arg2);
+            }
+            else if (zadanie == 2)
+            {
+                textBlock4.Text = textBox.Text + " * " + textBox1.Text;
+                textBlock5.Text = Convert.ToString(arg1 * arg2);
+            }
+            else if (zadanie == 3)
+            {
+                textBlock4.Text = textBox.Text + " / " + textBox1.Text;
+                textBlock5.Text = Convert.ToString(arg1 / arg2);
+            }
+            else if (zadanie == 4)
+            {
+                textBlock4.Text = textBox.Text + " % " + textBox1.Text;
+                textBlock5.Text = Convert.ToString(arg1 % arg2);
+            }
+
+        }
     }
 }
